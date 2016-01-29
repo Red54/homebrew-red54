91def77d7230a71b17ff35900680063a76c0f2fa
谢致邦 <Yeking@Red54.com>
2016-01-29 21:39:09 +0800

    zeal 0.2.1

---
+++ zeal.rb
@@ -0,0 +1,18 @@
+class Zeal < Formula
+  desc "Offline documentation browser inspired by Dash"
+  homepage "https://zealdocs.org/"
+  url "https://github.com/zealdocs/zeal/archive/v0.2.1.tar.gz"
+  version "0.2.1"
+  sha256 "994c5fad079cb34952fc05b0663920118489ed7f00f8e5a5887ff2e2bd05861d"
+
+  depends_on "qt5"
+  depends_on "libarchive"
+
+  def install
+    system "echo \"macx:INCLUDEPATH += #{HOMEBREW_PREFIX}/opt/libarchive/include\" >> src/src.pro"
+    system "echo \"macx:LIBS += -L#{HOMEBREW_PREFIX}/opt/libarchive/lib -larchive\" >> src/src.pro"
+    system "qmake"
+    system "make"
+    prefix.install "bin/Zeal.app"
+  end
+end
