a88d5e17be8c6bd6919417c04c69994b803bb453
谢致邦 <Yeking@Red54.com>
2016-02-23 17:22:05 +0800

    httpfs2 0.1.5

---
+++ httpfs2.rb
@@ -0,0 +1,31 @@
+class Httpfs2 < Formula
+  desc "FUSE filesystem for mounting files from http servers"
+  homepage "http://httpfs.sourceforge.net/"
+  url "https://sourceforge.net/projects/httpfs/files/httpfs2/httpfs2-0.1.5.tar.gz"
+  version "0.1.5"
+  sha256 "01cb4bb38deb344f540da6f1464dc7edbdeb51213ad810b8c9c282c1e17e0fc1"
+
+  depends_on "pkg-config" => :build
+  depends_on "dpkg" => :build
+  depends_on "asciidoc" => :build
+  depends_on "docbook-xsl" => :build
+  depends_on "gnutls"
+  depends_on :osxfuse
+
+  def install
+    system "make"
+    system "make"
+    bin.install "httpfs2"
+    bin.install "httpfs2-mt"
+    bin.install "httpfs2-ssl"
+    bin.install "httpfs2-ssl-mt"
+    man1.install "httpfs2.1"
+    man1.install "httpfs2-mt.1"
+    man1.install "httpfs2-ssl.1"
+    man1.install "httpfs2-ssl-mt.1"
+  end
+
+  test do
+    system "httpfs2-ssl-mt"
+  end
+end
