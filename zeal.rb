b4bab952f6160c8963d4530f3a4b14817be50940
谢致邦 (XIE Zhibang) <Yeking@Red54.com>
2016-11-01 19:44:41 +0800

    zeal 0.3.1

--- zeal.rb
+++ zeal.rb
@@ -1,20 +1,16 @@
 class Zeal < Formula
   desc "Offline documentation browser inspired by Dash"
   homepage "https://zealdocs.org/"
-  url "https://github.com/zealdocs/zeal/archive/v0.2.1.tar.gz"
-  sha256 "994c5fad079cb34952fc05b0663920118489ed7f00f8e5a5887ff2e2bd05861d"
-  patch do
-    url "https://github.com/zealdocs/zeal/commit/b26d655a7ab2c1ba4fc6212b8a1e019e98e1398b.patch"
-    sha256 "c8a95f5de7ebf997031ce09fc8cfd33631e37f27f0824b3a62225136a2c318b6"
-  end
-  patch :DATA
+  url "https://github.com/zealdocs/zeal/archive/v0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "55f8511977818612e00ae87a4fddaa346210189531469690f2e3961bb4c2c318"
 
   depends_on "qt5"
   depends_on "libarchive"
 
   def install
-    system "echo \"macx:INCLUDEPATH += #{HOMEBREW_PREFIX}/opt/libarchive/include\" >> src/src.pro"
-    system "echo \"macx:LIBS += -L#{HOMEBREW_PREFIX}/opt/libarchive/lib -larchive\" >> src/src.pro"
+    system "echo \"macx:INCLUDEPATH += #{HOMEBREW_PREFIX}/opt/libarchive/include\" >> qmake/common.pri"
+    system "echo \"macx:LIBS += -L#{HOMEBREW_PREFIX}/opt/libarchive/lib -larchive\" >> qmake/common.pri"
     system "qmake"
     system "make"
     prefix.install "bin/Zeal.app"
@@ -24,17 +20,3 @@ class Zeal < Formula
     system "false"
   end
 end
-__END__
-diff --git a/src/src.pro b/src/src.pro
-index 3dc66ac..f7ff023 100644
---- a/src/src.pro
-+++ b/src/src.pro
-@@ -4,7 +4,7 @@ QT += gui widgets sql
- CONFIG += c++11 silent
- 
- # Build features
--webengine {
-+greaterThan(QT_VERSION, 5.5) {
-     QT += webenginewidgets
-     DEFINES += USE_WEBENGINE
- } else {
